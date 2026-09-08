f82b6b62ad3278cc6ef05cd85332b004195d1f42
bryan-my <bry.jimenez@duocuc.cl>
2026-09-08 18:14:11 -0300

    Cambios

--- front_despacho/src/componentes/CrudAdmin/FormDespacho.jsx
+++ front_despacho/src/componentes/CrudAdmin/FormDespacho.jsx
@@ -2,6 +2,9 @@ import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
 import axios from "axios";
 
+const API_URL_VENTAS = import.meta.env.VITE_API_URL_VENTAS;
+const API_URL_DESPACHOS = import.meta.env.VITE_API_URL_DESPACHOS;
+
 export const FormDespacho = ({ venta, onClose }) => {
   const { register, handleSubmit } = useForm();
 
@@ -25,7 +28,7 @@ export const FormDespacho = ({ venta, onClose }) => {
 
     try {
       await axios.put(
-        `http://192.168.30/api/v1/ventas/${venta.idVenta}`,
+        `${API_URL_VENTAS}/api/v1/ventas/${venta.idVenta}`,
         jsonDataSales,
         {
           headers:{
@@ -34,7 +37,7 @@ export const FormDespacho = ({ venta, onClose }) => {
       }
         }
       );
-      await axios.post("http://192.168.320/api/v1/despachos", jsonData, {
+      await axios.post(`${API_URL_DESPACHOS}/api/v1/despachos`, jsonData, {
         headers:{
           'Content-Type': 'application/json',
           'Accept': 'application/json'
